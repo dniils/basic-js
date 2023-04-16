8cfc76b5c57a02bfc9999b2f64485806dd3f05cf
Daniil Shitikov <tochka098@gmail.com>
2023-04-16 23:18:53 +0400

    feat: add solutions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,7 +11,30 @@ const { NotImplementedError } = require("../extensions/index.js");
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {}
+function getCommonCharacterCount(s1, s2) {
+  const freq1 = {};
+  const freq2 = {};
+  let commonCount = 0;
+
+  // count frequency of each character in s1
+  for (let i = 0; i < s1.length; i++) {
+    freq1[s1[i]] = (freq1[s1[i]] || 0) + 1;
+  }
+
+  // count frequency of each character in s2
+  for (let i = 0; i < s2.length; i++) {
+    freq2[s2[i]] = (freq2[s2[i]] || 0) + 1;
+  }
+
+  // find common characters and count their occurrence
+  for (let char in freq1) {
+    if (freq2[char]) {
+      commonCount += Math.min(freq1[char], freq2[char]);
+    }
+  }
+
+  return commonCount;
+}
 
 module.exports = {
   getCommonCharacterCount,
